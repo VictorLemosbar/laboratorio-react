6f067aac071434d2fa6ee29b551071e0157ba415
VictorLemosbar <victor.escolas9045@gmail.com>
2025-12-23 20:12:30 -0300

    Error mensage added on Form

--- src/Pages/Activities/SimpleForm/Form.jsx
+++ src/Pages/Activities/SimpleForm/Form.jsx
@@ -1,13 +1,64 @@
-const Form = () => {
+import { useEffect, useState } from "react";
 
+const Form = () => {
+    const [submit, setSubmit] = useState({
+        cpf: "",
+        name: "",
+        passworld: ""
+    })
+
+    const [isNull, setIsNull] = useState(false)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+    }
+
+    function validationNull() {
+        if (submit.cpf == "" || submit.name == "" || submit.passworld == "") {
+            setIsNull(true)
+        }
+    }
+    function handleData(e) {
+
+        const { name, value } = e.target
+
+        setSubmit(prev => ({
+            ...prev,
+            [name]: value,
+        }))
+
+    }
 
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
 
-            <input type="text" placeholder="Nome completo"></input>
-            <input type="text" placeholder="CPF"></input>
-            <input type="password" placeholder="Senha"></input>
+            <input
+                name="name"
+                type="text"
+                placeholder="Nome completo"
+                value={submit.name}
+                onChange={handleData}
+            ></input>
+            <input
+                name="cpf"
+                type="text"
+                placeholder="CPF"
+                value={submit.cpf}
+                onChange={handleData}
+            ></input>
+            <input
+                name="passworld"
+                type="password"
+                placeholder="Senha"
+                value={submit.passworld}
+                onChange={handleData}
+            ></input>
+
+            <button className="btn" onClick={validationNull}>Submit</button>
+
+            {isNull && <span className="error-mensage">Preencha todos os campos!!</span>}
 
         </form>
     )
